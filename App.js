c6bae22e6d418d717551c96415fa517cc13faa32
Ojas Gupta <42967977+ThisIsOJAS@users.noreply.github.com>
2024-06-29 07:54:44 +0530

    Episode - 03

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const JSXheading = <h1 className="heading">Namaste React-JSX</h1>;
+// const heading = <h1 className="heading">Namaste React-JSX</h1>;
+
+const Title = () => <h1 className="heading">Namaste React-JSX</h1>;
+
+// This is is component composition
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">This is a React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(JSXheading);
+// root.render(heading);
+
+root.render(<HeadingComponent />);
